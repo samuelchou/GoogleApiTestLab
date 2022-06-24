3064cb2c92a97d88b6db325f853e0125c5e05d9f
Samuel Chou <samuelchou@hotmail.com.tw>
2022-06-24 16:46:08 +0800

    Add more api.

--- functions/index.js
+++ functions/index.js
@@ -9,3 +9,25 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text;
+  const writeResult = await admin.firestore().collection('messages').add({original: original});
+  res.json({result: `Message with ID: ${writeResult.id} addeddd.`});
+});
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snap, context) => {
+        const original = snap.data().original;
+        functions.logger.log('Uppercasing', context.params.documentId, original);
+        const uppercase = original.toUpperCase();
+
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as writing to Firestore.
+        // Setting an 'uppercase' field in Firestore document returns a Promise.
+        return snap.ref.set({uppercase}, {merge: true});
+    });
+
